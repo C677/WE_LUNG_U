15846365316c4e9dec710deb1439a52a3b7081ad
lee minju <dptmfkdls120@gmail.com>
2020-08-02 17:06:29 +0900

    check.html

--- we_Lung_u_flask/app.py
+++ we_Lung_u_flask/app.py
@@ -1,9 +1,9 @@
 #import os
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
+#Bootstrap(app)
 
 @app.route('/')
 def we_lung_u():
